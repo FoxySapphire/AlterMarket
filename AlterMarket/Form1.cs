7ae0322e03c96c8bbc9e91567f93b8541816327b
FoxySapphire <xfoxysapphirex@gmail.com>
2014-04-30 04:08:03 +0200

    Embedded references.

--- AlterMarket/Form1.cs
+++ AlterMarket/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,24 @@ namespace AlterMarket
     {
         public Form1()
         {
+            #region - Detect Dll Files -
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                var resourceName = new AssemblyName(args.Name).Name + ".dll";
+                var resource = Array.Find(GetType().Assembly.GetManifestResourceNames(),
+                                          element => element.EndsWith(resourceName));
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+                {
+                    var assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+
+            #endregion
+
             InitializeComponent();
         }
     }
